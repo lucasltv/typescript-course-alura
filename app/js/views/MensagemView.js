e97005512045394d858913348ed9325237959a59
Lucas Vieira <lucasltv@gmail.com>
2019-12-03 22:22:34 -0300

    Adjust

--- app/js/views/MensagemView.js
+++ app/js/views/MensagemView.js
@@ -1,10 +1,13 @@
 class MensagemView extends View {
-    // update(text: string): void {
-    //     this._elemento.innerHTML = `<p class="alert alert-info">${text}</p>`;
-    //     this._elemento.setAttribute("style", 'display: block');
-    //     setTimeout(this.hide.bind(this), 2000);
-    // }
+    update(text) {
+        this._elemento.innerHTML = this.template(text);
+        this._elemento.setAttribute("style", 'display: block');
+        setTimeout(this.hide.bind(this), 2000);
+    }
     template(text) {
         return `<p class="alert alert-info">${text}</p>`;
+    }
+    hide() {
+        this._elemento.setAttribute("style", 'display: none');
     }
 }
--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,16 +1,16 @@
 class MensagemView extends View<string> {
 
-    // update(text: string): void {
-    //     this._elemento.innerHTML = `<p class="alert alert-info">${text}</p>`;
-    //     this._elemento.setAttribute("style", 'display: block');
-    //     setTimeout(this.hide.bind(this), 2000);
-    // }
+    update(text: string): void {
+        this._elemento.innerHTML = this.template(text);
+        this._elemento.setAttribute("style", 'display: block');
+        setTimeout(this.hide.bind(this), 2000);
+    }
 
     template(text: string): string {
         return `<p class="alert alert-info">${text}</p>`;
     }
 
-    // hide(): void {
-    //     this._elemento.setAttribute("style", 'display: none');
-    // }
+    hide(): void {
+        this._elemento.setAttribute("style", 'display: none');
+    }
 }
