4a339f6b07c93c07037c0be87e77f6ee7d7983f0
Lucas Vieira <lucasltv@gmail.com>
2019-12-16 14:04:08 -0300

    Optional parameters

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,15 +1,19 @@
 export abstract class View<T> {
 
     protected _elemento: JQuery;
+    protected _escapar: boolean;
 
-    constructor(seletor: string) {
+    constructor(seletor: string, escapar: boolean = false) { //Valor default = false
         this._elemento = $(seletor);
+        this._escapar = escapar;
     }
 
     update(model: T): void {
-        this._elemento.html(this.template(model));
+        let template = this.template(model);
+        if (this._escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/g, "");
+        }
+        this._elemento.html(template);
     }
-
     abstract template(model: T): string; //Método abstrato: filhas são obrigadas a implementar
-
 }
